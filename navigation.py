ddfc2deefef93042b36f3707d9bfd79b2ea7ee30
RamanKawani <RamanKhalid888@gmail.com>
2024-12-15 22:54:03 +0000

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -2,33 +2,28 @@ import streamlit as st
 from home import home_section
 from upload import upload_papers
 from display import display_papers
-from user_profile import create_user_profile, view_and_update_profiles
+from user_profile import user_profile_section
 from guidelines import display_guidelines
 from collaborative_project import collaborative_project_section
-from institutional_partnership import institutional_partnership_section  # Assuming you added this section
 
-# Function to handle navigation between sections
-def app_navigation():
-    # Sidebar for navigation
+# Function to manage the navigation between different sections
+def app_navigation(df):
     st.sidebar.title("Kurdistan Research Exchange")
-
-    options = ["Home", "Upload Papers", "View Papers", "User Profile", "Submission Guidelines", "Collaborative Projects", "Institutional Partnerships"]
+    
+    # Sidebar navigation options
+    options = ["Home", "Upload Papers", "View Papers", "User Profile", "Submission Guidelines", "Collaborative Projects"]
     choice = st.sidebar.selectbox("Select a section", options)
 
-    # Based on the user's selection, navigate to the appropriate section
+    # Navigate based on user selection
     if choice == "Home":
         home_section()
     elif choice == "Upload Papers":
-        upload_papers()
+        upload_papers(df)
     elif choice == "View Papers":
-        display_papers()
+        display_papers(df)
     elif choice == "User Profile":
         user_profile_section()
     elif choice == "Submission Guidelines":
         display_guidelines()
     elif choice == "Collaborative Projects":
-        collaborative_project_section(user_email="user@example.com")
-    elif choice == "Institutional Partnerships":
-        institutional_partnership_section()  # Added the institutional partnership section here
-
-
+        collaborative_project_section(user_email="user@example.com")  # Pass the user email for interaction
