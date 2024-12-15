ce15feb5073b3134f4a3a27536f9616c5b2db2eb
RamanKawani <RamanKhalid888@gmail.com>
2024-12-15 18:00:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import streamlit as st
+from home import display_home
+from upload import display_upload
+
 def main():
     st.sidebar.title("Navigation")
     selection = st.sidebar.radio("Go to", ["Home", "Upload Research Paper"])
@@ -5,7 +9,7 @@ def main():
     if selection == "Home":
         display_home()
     elif selection == "Upload Research Paper":
-        display_upload()  # Use display_upload here
+        display_upload()
 
 if __name__ == "__main__":
     main()
