bcfce4ecb52fa4556ed8ed1d6fe644b42cd46573
RamanKawani <RamanKhalid888@gmail.com>
2024-12-16 15:05:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
-from navigation import app_navigation  # Ensure this is the correct import
+from navigation import app_navigation  # Import the updated navigation handler
 
 def main():
+    # Initialize the app with navigation
     app_navigation()  # This is the main navigation handler
 
-# Run the main function
+# Run the main function when the script is executed
 if __name__ == "__main__":
     main()
