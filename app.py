ba6672bc88eff33136f020962aab30582cd4df81
RamanKawani <RamanKhalid888@gmail.com>
2024-12-22 15:22:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 import streamlit as st
 from navigation import app_navigation  # Import the updated navigation handler
 
-# Access the GitHub token securely from Streamlit's secrets
-github_token = st.secrets.get("GITHUB_TOKEN")
-
 def main():
-    # Check if the token was successfully retrieved
-    if github_token:
-        st.sidebar.write(f"GitHub Token: {github_token[:5]}...")  # Display part of it for debugging (do not expose full token)
-    else:
-        st.sidebar.error("GitHub token not found.")
+    # Access the GitHub token securely from Streamlit's secrets
+    github_token = st.secrets.get("GITHUB_TOKEN")
+    
+    # Ensure the GitHub token is available for use but don't display it
+    if not github_token:
+        st.sidebar.error("GitHub Token is not available.")
     
     # Initialize the app with navigation
     app_navigation()  # This is the main navigation handler
