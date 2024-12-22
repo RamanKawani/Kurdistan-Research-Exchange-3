289518f2d6e4f303944cc76bc174ea3feab79b9a
RamanKawani <RamanKhalid888@gmail.com>
2024-12-22 14:44:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ import streamlit as st
 from navigation import app_navigation  # Import the updated navigation handler
 
 def main():
+    # Access GitHub token and repository URL from Streamlit's secrets
+    github_token = st.secrets["GITHUB_TOKEN"]
+    github_repo = st.secrets["GITHUB_REPO"]
+
+    # Example of using the GitHub token and repository
+    st.write(f"GitHub Repository: {github_repo}")
+    st.write(f"GitHub Token: {github_token[:5]}...")  # Show part of the token for security
+
     # Initialize the app with navigation
     app_navigation()  # This is the main navigation handler
 
