809611f276e09bc774c136780d8b978c1a29a519
RamanKawani <RamanKhalid888@gmail.com>
2024-12-22 15:53:31 +0300

    Update database.py

--- database.py
+++ database.py
@@ -8,8 +8,8 @@ def load_data():
     try:
         return pd.read_csv(DATA_FILE)
     except FileNotFoundError:
-        # If the file doesn't exist, return an empty DataFrame
-        return pd.DataFrame(columns=['title', 'author', 'university', 'year', 'abstract'])
+        # If the file doesn't exist, return an empty DataFrame with the correct columns
+        return pd.DataFrame(columns=['Title', 'Author', 'University', 'Year', 'Category', 'Link', 'PDF'])
 
 # Function to save data to the CSV file
 def save_data(df):
