9a77bb9238369989a4814560f212af0d123628ec
RamanKawani <RamanKhalid888@gmail.com>
2024-12-22 15:53:06 +0300

    Update data.py

--- data.py
+++ data.py
@@ -4,8 +4,14 @@ import pandas as pd
 def load_paper_data():
     # Specify the path to your CSV file (update if necessary)
     file_path = "research_papers.csv"
-    # Load the data from CSV
-    paper_df = pd.read_csv(file_path)
-
-    # Optionally, you can perform any data processing here if needed
+    
+    try:
+        # Load the data from CSV
+        paper_df = pd.read_csv(file_path)
+    except FileNotFoundError:
+        # Handle if the CSV file is not found
+        paper_df = pd.DataFrame(columns=['Title', 'Author', 'University', 'Year', 'Category', 'Link', 'PDF'])
+        print(f"File not found: {file_path}")
+    
+    # Return the loaded or empty DataFrame
     return paper_df
