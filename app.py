8d22ee512318a1e422b1cc062add9f7d4eca4d5f
RamanKawani <RamanKhalid888@gmail.com>
2025-01-10 09:14:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import streamlit as st
 from navigation import app_navigation  # Import the updated navigation handler
-from config import GITHUB_TOKEN
+from config import GITHUB_TOKEN  # Import the GitHub token from config.py
 
 def main():
+    # Display the GitHub token in the Streamlit app (just for testing, remove it later for security)
+    if GITHUB_TOKEN:
+        st.write("GitHub Token Loaded Successfully!")
+    else:
+        st.error("GitHub Token Not Found!")
+
     # Initialize the app with navigation
     app_navigation()  # This is the main navigation handler
 
 # Run the main function when the script is executed
 if __name__ == "__main__":
-    main() 
-
-
+    main()
